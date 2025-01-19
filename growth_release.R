dfab616793acf81fc2e3c2b4dbb5cf88f4fbbe4e
RonaldVisser <38214052+RonaldVisser@users.noreply.github.com>
2025-01-19 12:55:27 +0100

    update chart

--- growth_release.R
+++ growth_release.R
@@ -18,10 +18,10 @@ ggplot(series_tib, aes(x=year, y=GEP00281)) + geom_line()
 series_melt <- melt(series_tib, id.vars ="year", value.name = "ringwidth", variable.name = "series")
 
 ggplot(series_melt, aes(year,ringwidth, col=series)) +
-  geom_point(size = 0.75) + geom_line() + ylab("tree-ring width")
+  geom_point(size = 0.75) + geom_line() + ylab("tree-ring width (mm)")
 
 ggplot(series_melt, aes(year,ringwidth, col=series)) +
-  geom_point(size = 0.75) + geom_line() + ylab("tree-ring width") +
+  geom_point(size = 0.75) + geom_line() + ylab("tree-ring width (mm)") +
   ylim(0,6) + annotate("rect", xmin = 323, xmax = 330, ymin = 0, ymax = 6, alpha = .3) +
   theme(legend.position = c(0.1,0.85))
 ggsave("export/CUY_GEP_growth_release.png", width = 8, height = 6)
